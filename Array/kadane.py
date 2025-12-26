2473556f5d75199fed210d073b0b7ba7c95ea885
parshik04 <parshikchovatiya14@gmail.com>
2025-12-26 17:26:54 +0530

    kadane algorithm

--- Array/kadane.py
+++ Array/kadane.py
@@ -11,8 +11,36 @@ def basic_kadane(arr):
     return max_sum
 
 
+def kadane_with_indices(arr):
+    if not arr:
+        return 0,-1,-1
+    
+    current_sum = 0 
+    max_sum = float('-inf')
+    start = 0 
+    end = 0
+    temp_start= 0
+
+    for i in range(len(arr)):
+        current_sum += arr[i]
+
+        if current_sum > max_sum:
+            max_sum = current_sum
+            start = temp_start
+            end = i
+        if current_sum < 0 :
+            current_sum = 0
+            temp_start = i+1
+        
+    return max_sum,start,end
+
 if __name__ == "__main__":
     arr1 = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
     print(f"Array: {arr1}")
     max_sum = basic_kadane(arr1)
     print(max_sum)
+
+    arr2 = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    print(f"Array: {arr2}")
+    print(f"Max Sum: {kadane_with_indices(arr2)}")
+    print()
