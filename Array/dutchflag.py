a43c495c491b204cb37169e007165ffffb461b52
parshik04 <parshikchovatiya14@gmail.com>
2025-12-26 18:14:54 +0530

    dutch flag algorithm

---
+++ Array/dutchflag.py
@@ -0,0 +1,25 @@
+def dutchFlag(arr):
+    n = len(arr)
+    low = 0 
+    mid = 0 
+    high = n-1
+
+    while mid <= high :
+        if(arr[mid] == 0):
+            arr[low],arr[mid] = arr[mid],arr[low]
+            low += 1 
+            mid += 1
+
+        elif(arr[mid] == 1) :
+            mid+=1
+        
+        else:
+            arr[mid],arr[high] = arr[high],arr[mid]
+            high -= 1
+    return arr
+
+
+if __name__ == "__main__":
+    arr = [0,1,2,0,1,2,0,1,2]
+    arrs = dutchFlag(arr)
+    print(arrs)
